7d235d6b06f139620ffd74e11625d0d677d573f5
Adrien <adrien8255@aol.com>
2012-07-04 00:31:18 +0200

    Ajout de la gestion des intervalles

--- sources/CNR/CNR/Event.m
+++ sources/CNR/CNR/Event.m
@@ -49,7 +49,6 @@
 	if (range.location != NSNotFound) {
 		NSString* preStrDate = [summary substringWithRange:range];
 		
-		
 		NSArray* components = [preStrDate componentsSeparatedByString:@" "];
 		NSString* strDate =[NSString stringWithFormat:@"%@ %@ %@ %@", [components objectAtIndex:0],
 							[components objectAtIndex:1],
@@ -73,10 +72,46 @@
 		NSDate* d2 = [df dateFromString:strDate];
 		
 		NSArray* result = [NSArray arrayWithObjects:d, d2, nil];
-		
+        
 		return [result objectAtIndex:0];
 	}
-
+    else
+    {
+        removeUnused = @"([0-9]{1,2})[:space:]([:alpha:]+.)[:space:]([0-9]{4}) au ([:alpha:]+.)[:space:]([0-9]{1,2})[:space:]([:alpha:]+.)[:space:]([0-9]{4})";
+        
+        regex = [NSRegularExpression 
+                 regularExpressionWithPattern:removeUnused
+                 options:0
+                 error:nil];
+        
+        range = [regex rangeOfFirstMatchInString:summary options:0 range:NSMakeRange(0, [summary length])];
+        
+        NSString* preStrDate = [summary substringWithRange:range];
+        
+        NSArray* components = [preStrDate componentsSeparatedByString:@" "];
+		NSString* strDate =[NSString stringWithFormat:@"%@ %@ %@", [components objectAtIndex:0],
+							[components objectAtIndex:1],
+							[components objectAtIndex:2]];
+        
+        //Conversion en date
+		
+		NSDateFormatter* df = [[NSDateFormatter alloc] init];
+		[df setDateFormat:@"d MMM yyyy"];
+		[df setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"fr_FR"]];
+		
+		NSDate* d = [df dateFromString:strDate];
+		
+		
+		strDate =[NSString stringWithFormat:@"%@ %@ %@", [components objectAtIndex:0],
+				  [components objectAtIndex:1],
+				  [components objectAtIndex:2]];
+		
+		NSDate* d2 = [df dateFromString:strDate];
+		
+		NSArray* result = [NSArray arrayWithObjects:d, d2, nil];
+        
+		return [result objectAtIndex:0];
+    }
 	return nil;
 }
 
