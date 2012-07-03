97e692b0d698737a3ada2b336ba062ce16ab1f54
Adrien <adrien8255@aol.com>
2012-07-03 22:31:09 +0200

    Classement des événements en ordre ascendant (ça me parait plus logique)

--- sources/CNR/CNR/EventRepository.m
+++ sources/CNR/CNR/EventRepository.m
@@ -191,4 +191,106 @@ static EventRepository* __sharedEventRepository = nil;
     return  [strDate stringByReplacingCharactersInRange:NSMakeRange(0, 1) withString:[[strDate substringWithRange:NSMakeRange(0, 1)] capitalizedString]];
 }
 
++(NSArray *)loadFromCoreData {
+	
+	ManageCoreData* mcd = [[ManageCoreData alloc] init];
+	NSEntityDescription* entity = [mcd getEventEntity];
+	
+	NSFetchRequest* request = [[NSFetchRequest alloc] init];
+	
+	[request setEntity:entity];
+	
+	NSError* error = nil;
+	NSMutableArray* result = [[[mcd managedObjectContext] executeFetchRequest:request error:&error] mutableCopy];
+	
+	NSSortDescriptor* sortDescDate = [[NSSortDescriptor alloc] initWithKey:@"date" ascending:YES selector:@selector(compare:)];
+	NSSortDescriptor* sortDescTitle = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+	
+	[result sortUsingDescriptors:[NSArray arrayWithObjects:sortDescDate, sortDescTitle, nil]];
+	
+	NSMutableArray* events = [NSMutableArray array];
+	Event* lastObj = nil;
+	if ([result count]) {
+		lastObj = [result objectAtIndex:0];
+		[events addObject:[NSMutableArray arrayWithObject:lastObj]];
+	}
+	int currentIndex = 0;
+	
+	for (int i = 1; i < [result count]; i++) {
+		
+		Event* currentObj = [result objectAtIndex:i];
+		
+		NSCalendar *cal = [NSCalendar currentCalendar];
+		NSDateComponents* components = [[NSCalendar currentCalendar] components:NSEraCalendarUnit|NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit
+																	   fromDate:[currentObj date]];
+		NSDate* currentObjDate = [cal dateFromComponents:components];
+		
+		components = [[NSCalendar currentCalendar] components:NSEraCalendarUnit|NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit
+													 fromDate:[lastObj date]];
+		NSDate* lastObjDate = [cal dateFromComponents:components];
+		
+		if ([currentObjDate isEqualToDate:lastObjDate]) {
+			[[events objectAtIndex:currentIndex] addObject:currentObj];
+		}
+		else {
+			[events addObject:[NSMutableArray arrayWithObject:currentObj]];
+			currentIndex++;
+		}
+		
+		lastObj = currentObj;
+	}
+	
+	return events;
+}
+
++(BOOL)isInCoreData:(Event*)event {
+	
+	ManageCoreData* mcd = [[ManageCoreData alloc] init];
+	
+	NSEntityDescription* entity = [mcd getEventEntity];
+	
+	NSFetchRequest* request = [[NSFetchRequest alloc] init];
+	
+	[request setEntity:entity];
+	
+	NSPredicate* predicate =  [NSPredicate predicateWithFormat:@"(idE == %@)", [event idE]];
+	[request setPredicate:predicate];
+	
+	NSError* error = nil;
+	NSUInteger count = [[mcd managedObjectContext] countForFetchRequest:request error:&error];
+	
+        // Si plus d'un article ayant le même post link c'est qu'il est en doublon
+	if (count > 1) {
+		return YES;
+	}
+	return NO;
+}
+
++(NSArray*)getThreeFutureEvents {
+	
+	ManageCoreData* mcd = [[ManageCoreData alloc] init];
+	NSEntityDescription* entity = [mcd getEventEntity];
+	
+	NSFetchRequest* request = [[NSFetchRequest alloc] init];
+	[request setEntity:entity];
+	
+	[request setFetchLimit:3];
+	
+	NSDate* today = [NSDate date];
+	NSPredicate* predicate = [NSPredicate predicateWithFormat:@"(date >= %@)", today];
+	
+	NSLog(@"%@", predicate);
+	
+	[request setPredicate:predicate];
+	
+	NSError* error = nil;
+	NSMutableArray* result = [[[mcd managedObjectContext] executeFetchRequest:request error:&error] mutableCopy];
+	
+	NSSortDescriptor* sortDescDate = [[NSSortDescriptor alloc] initWithKey:@"date" ascending:NO selector:@selector(compare:)];
+	NSSortDescriptor* sortDescTitle = [[NSSortDescriptor alloc] initWithKey:@"title" ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+	
+	[result sortUsingDescriptors:[NSArray arrayWithObjects:sortDescDate, sortDescTitle, nil]];
+	
+	return result;
+}
 @end
